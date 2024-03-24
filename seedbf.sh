9f092e6785f4fb82f95091f82b159d5391939cec
AnuBitux <122746698+AnuBitux@users.noreply.github.com>
2024-03-24 22:30:06 +0100

    Update seedbf.sh

--- seedbf.sh
+++ seedbf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo ' '
+figlet SeedBF
+echo ' '
 echo seedbf tries to bruteforce a mnemonic seed passphrase
 echo "to run it use seedbf.py -p /home/anubitux/path_to_file/passphraselist.txt -s 'abandon abandon other words'"
 echo ' '
